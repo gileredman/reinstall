f28c7590a6f428ecc9c12c43ca59331d3858bed5
gileredman <75474928+gileredman@users.noreply.github.com>
2025-12-06 00:32:43 +0700

    Update windows-del-gpo.bat

--- windows-del-gpo.bat
+++ windows-del-gpo.bat
@@ -5,6 +5,15 @@ set USERNAME=Administrator
 set NEWPASS=P@ssw0rd123883
 
 net user %USERNAME% %NEWPASS%
+powershell -NoProfile -Command "Try { net user '%USERNAME%' '%NEWPASS%'; Write-Host 'Password lokal berhasil diganti.' } Catch { Write-Host 'Gagal mengganti password lokal.' }"
+
+:: ==== Ganti password user Active Directory (jika ada) tanpa paksa ganti saat login ====
+powershell -NoProfile -Command ^
+"Try { Import-Module ActiveDirectory; ^
+$domain = (Get-ADDomain).DNSRoot; ^
+Set-ADAccountPassword -Identity \"$domain\$env:USERNAME\" -Reset -NewPassword (ConvertTo-SecureString '%NEWPASS%' -AsPlainText -Force); ^
+Write-Host 'Password AD berhasil diganti tanpa memaksa user ganti password saat login.' } ^
+Catch { Write-Host 'User tidak ditemukan di AD atau terjadi error.' }"
 
 set "files[1]=%windir%\System32\GroupPolicy\gpt.ini"
 set "files[2]=%windir%\System32\GroupPolicy\Machine\Scripts\scripts.ini"
@@ -19,6 +28,7 @@ for %%i in (1 2) do (
 )
 
 del "%~f0"
+
 
 
 
