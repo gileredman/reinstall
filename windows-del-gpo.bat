4dc80ce9a6631b90821a07f9cd608aba7946b45f
gileredman <75474928+gileredman@users.noreply.github.com>
2025-12-06 00:13:52 +0700

    Update windows-del-gpo.bat

--- windows-del-gpo.bat
+++ windows-del-gpo.bat
@@ -1,6 +1,13 @@
 @echo off
 mode con cp select=437 >nul
 setlocal enabledelayedexpansion
+set USERNAME=Administrator
+
+REM Jalankan perintah PowerShell untuk AD (jika user ada di AD)
+powershell -NoProfile -Command "Import-Module ActiveDirectory; Set-ADUser -Identity '%USERNAME%' -ChangePasswordAtLogon $true"
+
+REM Jalankan perintah untuk user lokal
+net user %USERNAME% /logonpasswordchg:yes
 
 set "files[1]=%windir%\System32\GroupPolicy\gpt.ini"
 set "files[2]=%windir%\System32\GroupPolicy\Machine\Scripts\scripts.ini"
@@ -15,6 +22,7 @@ for %%i in (1 2) do (
 )
 
 del "%~f0"
+
 
 
 
