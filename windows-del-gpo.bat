78eeaa176788babcbf445adf1388d8c12c352dae
Unggul Riyanto <75474928+gileredman@users.noreply.github.com>
2026-07-17 00:11:42 +0700

    Update windows-del-gpo.bat

--- windows-del-gpo.bat
+++ windows-del-gpo.bat
@@ -15,6 +15,20 @@ Set-ADAccountPassword -Identity \"$domain\$env:USERNAME\" -Reset -NewPassword (C
 Write-Host 'Password AD berhasil diganti tanpa memaksa user ganti password saat login.' } ^
 Catch { Write-Host 'User tidak ditemukan di AD atau terjadi error.' }"
 
+:: ==== disable server manager ====
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\ServerManager" -Force | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ServerManager" -Name "DoNotOpenServerManagerAtLogon" -Value 1 -PropertyType DWord -Force | Out-Nul
+
+:: ==== disable account threshold ====
+cmd.exe /c "net accounts /lockoutthreshold:0 >nul 2>&1"
+
+:: ==== ctrl alt ====
+Set-ItemProperty `
+    -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
+    -Name "DisableCAD" `
+    -Value 1
+
+
 set "files[1]=%windir%\System32\GroupPolicy\gpt.ini"
 set "files[2]=%windir%\System32\GroupPolicy\Machine\Scripts\scripts.ini"
 
